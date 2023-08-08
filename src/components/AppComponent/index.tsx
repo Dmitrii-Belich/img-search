2604333c2f7df5afdb7c60e5eb79368b0efe7c3d
Дмитрий Белич <61316958+Dmitrii-Belich@users.noreply.github.com>
2023-08-08 11:39:51 +0300

    feat(core): replace store provider

--- src/components/AppComponent/index.tsx
+++ src/components/AppComponent/index.tsx
@@ -1,26 +1,18 @@
 import './index.css'
 
 import React from 'react'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-import {createStore, applyMiddleware} from 'redux'
-
-import rootReducer from '../../reducers'
 
 import MainComponent from '../MainComponent'
 import HeaderComponent from '../HeaderComponent'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
 
 function AppComponent() {
   return (
-    <Provider store={store}>
       <div className="App">
         <HeaderComponent></HeaderComponent>
 
         <MainComponent></MainComponent>
       </div>
-    </Provider>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import './index.css'
 
 import React from 'react'
+import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom/client'
+import {applyMiddleware, createStore} from 'redux'
+import thunk from 'redux-thunk'
 
 import AppComponent from './components/AppComponent'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
+
 root.render(
   <React.StrictMode>
-    <AppComponent/>
+    <Provider store={store}>
+      <AppComponent/>
+    </Provider>
   </React.StrictMode>
 )
